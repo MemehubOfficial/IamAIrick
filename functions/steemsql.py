f7f31747fc3c6a1491bbf3c6f5a39314e59cd91b
MemehubOfficial <thememehublol@gmail.com>
2019-08-15 08:56:56 -0400

    added context handler

--- functions/steemsql.py
+++ functions/steemsql.py
@@ -3,27 +3,34 @@ import functions.func as func
 import time
 import config
 import pypyodbc as db
+from contextlib import contextmanager
 
+@contextmanager
 def open_connection():
-    return db.connect('Driver={ODBC Driver 17 for SQL Server};'
+    try:
+        connection = db.connect('Driver={ODBC Driver 17 for SQL Server};'
                                 'Server=sql.steemsql.com;'
                                 'Database=DBSteem;'
                                 'uid='+config.steemsql_uid+';pwd='+config.steemsql_pwd)
+        yield connection
+    finally:
+        connection.close()
 
 #Queries steemsql with the query passed as a param
 #returns query data as a dataframe
 def query(query):
     now = time.time()
 
-    connection = open_connection()
+    #connection = open_connection()
 
     # cursor = connection.cursor()
     # data = cursor.execute(query).fetchall()
     # dataframe = pd.DataFrame(data)
 
-    dataframe = pd.read_sql(query, connection)
+    with open_connection() as connection:
+        dataframe = pd.read_sql(query, connection)
 
-    connection.close()
+    #connection.close()
 
     if dataframe.empty:
         print("Query is empty")
